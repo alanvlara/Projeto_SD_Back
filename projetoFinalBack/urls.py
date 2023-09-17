220a6200da118395e587faa2962361cc9190bb39
ALAN LARA <alanlara685@gmail.com>
2023-09-17 17:45:14 -0300

    Cors

--- projetoFinalBack/urls.py
+++ projetoFinalBack/urls.py
@@ -1,23 +1,8 @@
-"""
-URL configuration for projetoFinalBack project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 
-from rest_framework import permissions
+from rest_framework import permissions, routers
 from drf_yasg import openapi  
 from drf_yasg.views import get_schema_view  
 from django.conf import settings
@@ -26,7 +11,7 @@ from dj_rest_auth.views import PasswordResetConfirmView
 from utils.custom_view import CustomUserDetailsView
 from utils.custom_view_register import CustomRegisterView
 
-
+router = routers.DefaultRouter()
 schema_view = get_schema_view(
     openapi.Info(
         title='DesporPato API',
@@ -37,6 +22,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    # path("", include(router.urls)),
     path('admin/', admin.site.urls),
     path('usuario/', include('usuario.urls')),
     path('atividade/', include('atividade.urls')),
