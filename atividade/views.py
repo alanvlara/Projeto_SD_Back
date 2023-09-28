38f62549f1140e3f24beccbf66fd402595adf6ed
ALAN LARA <alanlara685@gmail.com>
2023-09-28 01:51:38 -0300

    atividades erros

--- atividade/views.py
+++ atividade/views.py
@@ -3,7 +3,6 @@ from usuario.views import UsuarioReadSerializer
 from rest_framework import serializers
 from rest_framework import viewsets
 from rest_framework.permissions import SAFE_METHODS
-from rest_framework.exceptions import ValidationError
 from utils.permissions import IsOwnerOrReadOnly
 from django.utils import timezone
 from pytz import timezone as pytz_timezone
@@ -31,12 +30,12 @@ class AtividadeWriteSerializer(serializers.ModelSerializer):
         #Verifica se a atividade existe
         atividade_existente = Atividade.objects.filter(evento=evento, usuario=usuario).first()
         if atividade_existente:
-            raise ValidationError("Já existe uma atividade para este evento e usuário.")
+            raise serializers.ValidationError("Já existe uma atividade para este evento e usuário.")
 
         # Pega a data e hora atual com o fuso horário de Brasília e verifica a data
         data_atual_brasilia = timezone.now().astimezone(brasilia_timezone).date()
         if data_evento and data_evento != data_atual_brasilia:
-            raise ValidationError("A atividade só pode ser criada para um evento que ocorre na data atual (hoje) em Brasília.")
+            raise serializers.ValidationError("A atividade só pode ser criada para um evento que ocorre na data atual (hoje) em Brasília.")
 
         if (evento.esporte == usuario.esportePreferido and usuario.is_active):
             usuario.totalEventos += 1
